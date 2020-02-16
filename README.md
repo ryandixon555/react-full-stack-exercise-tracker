c7077e05d8b29bac5488e080a51466a106463d63
rdixon555 <ryan555@sky.com>
2020-02-16 17:55:10 +0000

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+React Full Stack MERN Exercise Tracker
+
+## To Run
+- Navigate to the backend folder and run nodemon server
+- Run npm start in the root
+
+## Makes use of
+- ReactJS
+- React Router
+- MongoDB
+- ExpressJS
+- NodeJS
+- Bootstrap
+- Axios
+
+**Note: this may not work on your local due to MongoDB whitelisting**
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Navbar from "./components/navbar.component.js";
+import ExercisesList from "./components/exercises-list.component.js";
+import EditExercise from "./components/edit-exercises.component.js";
+import CreateExercise from "./components/create-exercise.component.js";
+import CreateUser from "./components/create-user.component.js";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
